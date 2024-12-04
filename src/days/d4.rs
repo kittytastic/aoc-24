06fad5ad63fe687afcde8415927e4130147e71ca
Jack Slingsby <Jack.Slingsby@imgtec.com>
2024-12-04 13:55:58 +0000

    Finish d4

--- src/days/d4.rs
+++ src/days/d4.rs
@@ -1,3 +1,64 @@
-pub fn day4_main(_second_part: bool, _extra_args: &Vec<String>){
+use std::char;
+
+use crate::utils::utils::{get_input_file, Grid, GridPoint};
+
+fn slice_is_xmas(slice: Option<Vec<char>>)->bool{
+    if let Some(v) = slice{
+        let v_str:String = v.into_iter().collect();
+        return v_str == "XMAS" || v_str == "SAMX"; 
+    }
+    false
+}
+
+pub fn part1(grid: &Grid<char>){
+    let mut match_count:usize = 0;
     
+    for y in 0..grid.height(){
+        for x in 0..grid.width(){
+            let point = grid.at(x, y).expect("In bounds");
+            if slice_is_xmas(point.right_slice(4)){match_count+=1}
+            if slice_is_xmas(point.down_slice(4)){match_count+=1}
+            if slice_is_xmas(point.down_right_slice(4)){match_count+=1}
+            if slice_is_xmas(point.down_left_slice(4)){match_count+=1}
+        }
+    }
+
+    println!("Answer Part 1: {}", match_count);
+}
+
+fn point_is_xmas(point: &GridPoint<char>) -> bool{
+    if *point.value() != 'A'{return false;}
+    let ul = if let Some(v) = point.up_left(){v.value().clone()}else{return false;};
+    let ur = if let Some(v) = point.up_right(){v.value().clone()}else{return false;};
+    let dl = if let Some(v) = point.down_left(){v.value().clone()}else{return false;};
+    let dr = if let Some(v) = point.down_right(){v.value().clone()}else{return false;};
+
+    let uphill_mas = (dl=='M' && ur=='S') || (dl=='S' && ur=='M');
+    let downhill_mas = (dr=='M' && ul=='S') || (dr=='S' && ul=='M');
+
+    uphill_mas && downhill_mas
+}
+
+fn part2(grid: &Grid<char>){
+    let mut match_count:usize = 0;
+    
+    for y in 0..grid.height(){
+        for x in 0..grid.width(){
+            let point = grid.at(x, y).expect("In bounds");
+            if point_is_xmas(&point){match_count+=1}
+        }
+    }
+
+    println!("Answer Part 2: {}", match_count);
+}
+
+pub fn day4_main(second_part: bool, _extra_args: &Vec<String>){
+    let input_file_str = get_input_file("d4.txt");
+    let grid = Grid::new_from_str(&input_file_str);
+    
+    if second_part{
+        part2(&grid);
+    }else{
+        part1(&grid);
+    }
 }
--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -11,3 +11,135 @@ pub fn get_input_file(file_name: &str) -> String{
 
     return fs::read_to_string(path).expect("Should be able to read file");
 }
+
+
+pub struct Grid<T> {
+    data: Vec<Vec<T>>,
+    height: usize,
+    width: usize
+}
+
+pub struct GridPoint<'a, T>{
+    grid: &'a Grid<T>,
+    x: usize,
+    y: usize
+}
+
+impl Grid<char>{
+    pub fn new_from_str(txt: &str) -> Grid<char>{
+        let input_data: Vec<Vec<char>> = txt.lines().map(|line: &str|{
+            line.chars().map(|c|c).collect::<Vec<char>>()
+        }).collect::<Vec<_>>();
+
+        Grid::new(input_data)
+    }
+}
+
+impl<T> Grid<T>{
+    pub fn new(data: Vec<Vec<T>>) -> Self{
+        let width = data[0].len();
+        let height = data.len();
+        Self {data, height, width}
+    }
+
+    pub fn height(&self)->usize{
+        self.height
+    }
+
+    pub fn width(&self)->usize{
+        self.width
+    }
+
+    pub fn at<'a>(&'a self, x:usize, y:usize) -> Option<GridPoint<'a, T>> {
+        if x>=self.width {return None;}
+        if y>=self.height {return None;}
+        Some(GridPoint {grid: &self, x, y})
+    } 
+}
+
+impl<'a, T> GridPoint<'a, T>{
+    pub fn value(&'a self) -> &'a T{
+        &self.grid.data[self.y][self.x]
+    } 
+
+    pub fn _left(&self) -> Option<GridPoint<T>>{
+        if self.x == 0 {return None}
+        self.grid.at(self.x-1, self.y)
+    }
+
+    pub fn _right(&self) -> Option<GridPoint<T>> {
+        if self.x == self.grid.width-1{return None}
+        self.grid.at(self.x+1, self.y)
+    }
+
+    pub fn _up(&self) -> Option<GridPoint<T>> {
+        if self.y == 0 {return None}
+        self.grid.at(self.x, self.y-1)
+    }
+
+    pub fn _down(&self) -> Option<GridPoint<T>> {
+        if self.y == self.grid.height-1{return None}
+        self.grid.at(self.x, self.y+1)
+    }
+    
+    pub fn up_left(&self) -> Option<GridPoint<T>>{
+        if self.x == 0 {return None}
+        if self.y == 0 {return None}
+        self.grid.at(self.x-1, self.y-1)
+    }
+
+    pub fn up_right(&self) -> Option<GridPoint<T>> {
+        if self.x == self.grid.width-1{return None}
+        if self.y == 0 {return None}
+        self.grid.at(self.x+1, self.y-1)
+    }
+
+    pub fn down_left(&self) -> Option<GridPoint<T>> {
+        if self.y == self.grid.height-1{return None}
+        if self.x == 0 {return None}
+        self.grid.at(self.x-1, self.y+1)
+    }
+
+    pub fn down_right(&self) -> Option<GridPoint<T>> {
+        if self.y == self.grid.height-1{return None}
+        if self.x == self.grid.width-1{return None}
+        self.grid.at(self.x+1, self.y+1)
+    }
+}
+
+impl<'a, T:Clone> GridPoint<'a, T>{
+    pub fn right_slice(&self, slice_len: usize) -> Option<Vec<T>> {
+        if self.x > self.grid.width-slice_len{return None}
+        Some((0..slice_len).into_iter().map(|d|{self.grid.data[self.y][self.x+d].clone()}).collect::<Vec<T>>())
+    }
+    
+    pub fn down_slice(&self, slice_len: usize) -> Option<Vec<T>> {
+        if self.y > self.grid.height-slice_len{return None}
+        Some((0..slice_len).into_iter().map(|d|{self.grid.data[self.y+d][self.x].clone()}).collect::<Vec<T>>())
+    }
+    
+    pub fn down_right_slice(&self, slice_len: usize) -> Option<Vec<T>> {
+        if self.y > self.grid.height-slice_len{return None}
+        if self.x > self.grid.width-slice_len{return None}
+        Some((0..slice_len).into_iter().map(|d|{self.grid.data[self.y+d][self.x+d].clone()}).collect::<Vec<T>>())
+    }
+    
+    pub fn down_left_slice(&self, slice_len: usize) -> Option<Vec<T>> {
+        if self.y > self.grid.height-slice_len{return None}
+        if self.x < slice_len-1{return None}
+        Some((0..slice_len).into_iter().map(|d|{self.grid.data[self.y+d][self.x-d].clone()}).collect::<Vec<T>>())
+    }
+} 
+
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_grid_slice_inbound() {
+        //assert_eq!(bad_add(1, 2), 3);
+    }
+}
