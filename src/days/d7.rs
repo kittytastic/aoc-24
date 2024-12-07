c6cc4f6a8466105d610f2fd656933e6b16cd4220
Jack Slingsby <jack@theslingsbys.com>
2024-12-07 15:19:40 +0000

    Tidy part 1

--- src/days/d7.rs
+++ src/days/d7.rs
@@ -13,7 +13,6 @@ pub fn day7_main(_second_part: bool, _extra_args: &Vec<String>){
     println!("Answer part 1: {}", part_one); 
 
     let part_two:u64 = input.iter().map(|(target, vals)|{if part2_step(0, *target, &vals){*target}else{0}}).sum();
-    //println!("ASDASD: {}", part2_step(0, 156, &vec![15, 6]))
     println!("Answer part 2: {}", part_two); 
 
 }
@@ -27,8 +26,9 @@ fn part1_step(val: u64, target: u64, remaining: &[u64])->bool{
     return using_plus | using_mul;
 }
 
-fn concat_base_10(low: u64, high: u64)->Option<u64>{
-    low.checked_add(u64::checked_pow(10, low.checked_ilog10()?+1)?.checked_mul(high)?)
+fn concat_base_10(low: u64, high: u64)->u64{
+    //low.checked_add(u64::checked_pow(10, low.checked_ilog10()?+1)?.checked_mul(high)?)
+    low + u64::pow(10, low.ilog10()+1) * high
 }
 
 
@@ -38,7 +38,7 @@ fn part2_step(val: u64, target: u64, remaining: &[u64])->bool{
     let next_val = remaining[0];
     let using_plus = part2_step(val+next_val, target, &remaining[1..]);
     let using_mul = part2_step(val*next_val, target, &remaining[1..]);
-    let using_concat = if let Some(c) = concat_base_10(next_val, val) {part2_step(c, target, &remaining[1..])}else{false};
+    let using_concat =  part2_step(concat_base_10(next_val, val), target, &remaining[1..]);
 
     return using_plus | using_mul | using_concat;
 }
@@ -50,9 +50,8 @@ mod tests {
 
     #[test]
     fn test_concat() {
-        println!("{}",u64::ilog10(6));
-        assert_eq!(concat_base_10(3, 4), Some(43));
-        assert_eq!(concat_base_10(10, 19), Some(1910));
-        assert_eq!(concat_base_10(6, 15), Some(156));
+        assert_eq!(concat_base_10(3, 4), 43);
+        assert_eq!(concat_base_10(10, 19), 1910);
+        assert_eq!(concat_base_10(6, 15), 156);
     }
 }
