5a81b1a8fd2629722ccfc740ee833feb5e866162
Jack Slingsby <jack@theslingsbys.com>
2024-12-17 16:47:37 +0000

    It's not pretty, but the star has been collected

--- src/days/d17.rs
+++ src/days/d17.rs
@@ -5,29 +5,19 @@ use crate::utils::utils::{get_input_file, get_number_substr};
 pub fn day17_main(_second_part: bool, _extra_args: &Vec<String>){
     let input_string = get_input_file("d17.txt");
     let l:Vec<&str> = input_string.lines().collect();
-    let mut reg_a:usize = get_number_substr(l[0])[0].parse().expect("Valid number");
-    let mut reg_b:usize = get_number_substr(l[1])[0].parse().expect("Valid number");
-    let mut reg_c:usize = get_number_substr(l[2])[0].parse().expect("Valid number");
+    let reg_a:usize = get_number_substr(l[0])[0].parse().expect("Valid number");
+    let reg_b:usize = get_number_substr(l[1])[0].parse().expect("Valid number");
+    let reg_c:usize = get_number_substr(l[2])[0].parse().expect("Valid number");
     let program:Vec<usize> = get_number_substr(l[4]).iter().map(|s|{s.parse().expect("Valid number")}).collect();
 
     println!("Reg a: {} b: {} c: {}; Program: {:?}", reg_a, reg_b, reg_c, program);
 
     let ops:Vec<(usize, usize)> = program.chunks(2).map(|c|(c[0], c[1])).collect();
-    let mut output: Vec<usize> = Vec::new();
-    let mut pc = 0;
-
-    for (op_code, operand) in ops.iter(){
-        println!("{}", op_to_str(*op_code, *operand));
-    }
-
-    while pc < ops.len(){
-        let (op_code, operand) = ops[pc];
-        //println!("[{}] {}", pc, op_to_str(op_code, operand));
-        let result = apply_op(op_code, operand, &mut pc, &mut reg_a, &mut reg_b, &mut reg_c);
-        if let Some(r) = result {output.push(r);}
-    }
-    let output_str = output.iter().map(|v|{v.to_string()}).collect::<Vec<_>>().join(",");
+    let part1_output = run_program(&ops, reg_a, reg_b, reg_c);
+    let output_str = part1_output.iter().map(|v|{v.to_string()}).collect::<Vec<_>>().join(",");
     println!("Answer to part 1: {}", output_str);
+
+    part2(&program);
 
 }
 
@@ -86,4 +76,105 @@ fn apply_op(op_code: usize, operand: usize, pc: &mut usize, reg_a: &mut usize, r
     }
 
     return_val
+}
+
+fn run_program(ops: &Vec<(usize, usize)>, mut reg_a: usize, mut reg_b: usize, mut reg_c: usize)->Vec<usize>{
+    let mut pc = 0;
+    let mut output: Vec<usize> = Vec::new();
+
+    /*for (op_code, operand) in ops.iter(){
+        println!("{}", op_to_str(*op_code, *operand));
+    }*/
+
+    while pc < ops.len(){
+        let (op_code, operand) = ops[pc];
+        //println!("[{}] {}", pc, op_to_str(op_code, operand));
+        let result = apply_op(op_code, operand, &mut pc, &mut reg_a, &mut reg_b, &mut reg_c);
+        if let Some(r) = result {output.push(r);}
+    }
+
+    output
+}
+
+fn part2(program: &Vec<usize>){
+    /*for a_shift in 0..8{
+        for a_low in 0..8{
+            let b1 = a_low;
+            let b2 = b1 ^ 1;
+            let c1 = a_shift;
+            let b3 = b2 ^ c1;
+            let b4 = b3 ^ 4;
+            println!("{} {} (takes {})-> {}", a_low, a_shift, b2, b4%8);
+        }
+    }*/
+    /*for c in 0..8{
+        for b in 0..8{
+            println!("b: {} c: {} = {}", b, c, b^c^4^1)
+        }
+    }*/
+    /*let solution = find_solution(15, 0, program);
+    println!("Answer to part 2: {:?}", solution);
+    if solution.is_none(){println!("Cannot get answer"); return;}
+    */
+    // Other: 202366627358720
+    // First: 202366115129871
+    println!("{:o}", 202366627358720 as usize);
+    println!("{:o}", 202366115129871 as usize);
+    let x = other_attempt2(program);
+    let ops:Vec<(usize, usize)> = program.chunks(2).map(|c|(c[0], c[1])).collect();
+    let part1_output = run_program(&ops, x, 0, 0);
+    let output_str = part1_output.iter().map(|v|{v.to_string()}).collect::<Vec<_>>().join(",");
+    println!("{}", output_str)
+}
+
+fn find_solution(digit_idx: usize, num: usize, target_digits: &Vec<usize>)->Option<usize>{
+    if digit_idx == 9 {println!("hi, {} {} {:?}", digit_idx, num, target_digits);}
+    for b in 0..8{
+        let b1 = b^1;
+        let c = num>>(b1+(digit_idx*3));
+        let b2 = b1 ^ c ^ 4;
+        let digit = b2 % 8;
+        if digit != target_digits[digit_idx]{continue;} // No match - try again
+        
+        let new_num = num + (b<<(3*digit_idx));
+        if digit_idx == 0 {return Some(new_num)} // Base case
+        let sub_solution = find_solution(digit_idx-1, new_num, target_digits);
+        if sub_solution.is_some() {return sub_solution;}
+    }
+
+    None
+}
+
+fn other_attempt(program: &Vec<usize>)->usize{
+    let ops:Vec<(usize, usize)> = program.chunks(2).map(|c|(c[0], c[1])).collect();
+    let mut a = 0;
+    for j in 0..16{
+        a*=8;
+        for i in 0..8{
+            let output = run_program(&ops, a+i, 0, 0);
+            if output == &program[(15-j)..]{
+                println!("{:?}, {:?}", output, &program[(15-j)..]);
+                a+=i;
+                break;
+                println!("Match!")
+            }
+        }
+    }
+
+    println!("{a}");
+    a
+}
+
+
+fn other_attempt2(program: &Vec<usize>)->usize{
+    let ops:Vec<(usize, usize)> = program.chunks(2).map(|c|(c[0], c[1])).collect();
+    for i in 0..(8*8*8*8){
+        let a = 0o5600644674020000 + i;
+            let output = run_program(&ops, a, 0, 0);
+            if output == *program{
+                println!("Match! {a}");
+                return a
+        }
+    }
+    return 0;
 }
