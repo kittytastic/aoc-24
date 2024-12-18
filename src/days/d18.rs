498394d947088674a8890d52f9edb30e5c8f6306
Jack Slingsby <jack@theslingsbys.com>
2024-12-18 17:38:24 +0000

    Part 2 donw

--- src/days/d18.rs
+++ src/days/d18.rs
@@ -1,42 +1,29 @@
-use std::{cmp::Ordering, collections::{BinaryHeap, HashSet}, hash::Hash};
+use std::{cmp::Ordering, collections::{BinaryHeap, HashSet}};
+
 
 use crate::utils::utils::{get_input_file, Direction, Grid, Point};
 
 pub fn day18_main(_second_part: bool, _extra_args: &Vec<String>){
     let input_string = get_input_file("d18.txt");
-    
-    let barriers: HashSet<Point> = input_string.lines().take(1024).map(|l|{
+    let height = 71;
+    let width = 71;
+    let barrier_start = 1024;
+
+    let all_barriers: Vec<Point> = input_string.lines().map(|l|{
         let s = l.split(',').collect::<Vec<_>>();
         Point::new(s[0].parse().expect("Valid number"), s[1].parse().expect("Valid number"))
     }).collect();
 
-    //println!("Barriers: {:?}", barriers);
-    let height = 71;
-    let width = 71;
-    let end = Point::new(width-1, height-1);
+    let p1_barriers:HashSet<Point> = all_barriers.iter().take(barrier_start).cloned().collect();
+    let p1_answer = traverse_maze(width, height, &p1_barriers).expect("Should find path");
+    println!("Answer to part 1: {}", p1_answer);
 
-    let mut to_visit: BinaryHeap<MinHeapContainer<usize, Point>> = BinaryHeap::new();
-    let mut visited: HashSet<Point> = HashSet::new();
-    let length;
-
-    to_visit.push(MinHeapContainer{weight: 0, value: Point::new(0, 0)});
-
-    loop {
-        let Some(next) = to_visit.pop() else {unreachable!("Failed to find end");};
-        //println!("{:?}", next.value);
-        if next.value == end {length = next.weight; break;}
-        if !visited.insert(next.value.clone()){continue;}
-
-        for d in vec![Direction::Up, Direction::Right, Direction::Down, Direction::Left]{
-            if let Some(p) = next.value.step_direction(d, width, height){
-                if visited.contains(&p){continue;}
-                if barriers.contains(&p){continue;}
-                to_visit.push(MinHeapContainer{weight: next.weight+1, value: p});
-            }
-        }
+    let mut p2_barriers: HashSet<Point> = HashSet::new();
+    for b in all_barriers.iter(){
+        p2_barriers.insert(b.clone());
+        let maze_result = traverse_maze(width, height, &p2_barriers);
+        if maze_result.is_none(){println!("Answer to part 2: {}", b); break;}
     }
-
-    println!("Answer to part 1: {}", length);
 
 }
 
@@ -60,5 +47,29 @@ impl<T: Ord, S: Ord> Ord for MinHeapContainer<T, S> {
 impl<T: Ord, S: Ord> PartialOrd for MinHeapContainer<T, S> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
+    }
+}
+
+fn traverse_maze(width: usize, height: usize, barriers: &HashSet<Point>)->Option<usize>{
+    let end = Point::new(width-1, height-1);
+
+    let mut to_visit: BinaryHeap<MinHeapContainer<usize, Point>> = BinaryHeap::new();
+    let mut visited: HashSet<Point> = HashSet::new();
+
+    to_visit.push(MinHeapContainer{weight: 0, value: Point::new(0, 0)});
+
+    loop {
+        let Some(next) = to_visit.pop() else {return None};
+        //println!("{:?}", next.value);
+        if next.value == end {return Some(next.weight);}
+        if !visited.insert(next.value.clone()){continue;}
+
+        for d in vec![Direction::Up, Direction::Right, Direction::Down, Direction::Left]{
+            if let Some(p) = next.value.step_direction(d, width, height){
+                if visited.contains(&p){continue;}
+                if barriers.contains(&p){continue;}
+                to_visit.push(MinHeapContainer{weight: next.weight+1, value: p});
+            }
+        }
     }
 }
