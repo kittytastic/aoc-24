bec2bbd851a5e7e81bbed304fc5893c09ed22ffe
Jack Slingsby <jack@theslingsbys.com>
2024-12-19 11:39:24 +0000

    Finished d19 pt2

--- src/days/d19.rs
+++ src/days/d19.rs
@@ -17,24 +17,48 @@ pub fn day19_main(_second_part: bool, _extra_args: &Vec<String>){
         }
     }
     println!("Answer to part 1: {}", i);
+
+    let mut j = 0;
+    for t in target_towels.iter(){
+        let mut visited: HashMap<&str, usize> = HashMap::new();
+        j += part2_can_be_made(t, &base_pattens, & mut visited)
+    }
+    println!("Answer to part 2: {}", j);
     
 }
 
-fn part1_can_be_made<'a>(pattern: &'a str, base_pattens: &Vec<&str>, visted: & mut HashMap<&'a str, bool>)->bool{
+fn part1_can_be_made<'a>(pattern: &'a str, base_pattens: &Vec<&str>, visited: & mut HashMap<&'a str, bool>)->bool{
     if pattern.len() == 0 {return true;}
-    if let Some(v) = visted.get(pattern) {return *v;};
+    if let Some(v) = visited.get(pattern) {return *v;};
     
     let mut possible = false;
     for p in base_pattens {
         if pattern.starts_with(p){
             let sub_pattern = &pattern[p.len()..];
-            if part1_can_be_made(sub_pattern, base_pattens, visted){
+            if part1_can_be_made(sub_pattern, base_pattens, visited){
                 possible = true;
                 break;
             }
         }
     }
 
-    visted.insert(pattern, possible);
+    visited.insert(pattern, possible);
     possible
+}
+
+
+fn part2_can_be_made<'a>(pattern: &'a str, base_pattens: &Vec<&str>, visited: & mut HashMap<&'a str, usize>)->usize{
+    if pattern.len() == 0 {return 1;}
+    if let Some(v) = visited.get(pattern) {return *v;};
+    
+    let mut combos = 0;
+    for p in base_pattens {
+        if pattern.starts_with(p){
+            let sub_pattern = &pattern[p.len()..];
+            combos += part2_can_be_made(sub_pattern, base_pattens, visited);
+        }
+    }
+
+    visited.insert(pattern, combos);
+    combos
 }
